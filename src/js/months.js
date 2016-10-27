54151663b1d649178cf80e35d749a6b1e42daa70
T-Zahil <thomas.sanlis@gmail.com>
2016-10-27 17:35:54 +0200

    [month] rebuild

--- src/js/months.js
+++ src/js/months.js
@@ -1,12 +1,12 @@
 setTimeout(function(){
     console.log('coucou');
     $('.month__imgWrapper').css({
-        'width': '70vw',
-        'height': '40vh',
+        'width': '68vw',
+        'height': '33vh',
         'align-self': 'flex-start',
         'position': 'absolute',
         'right': '0',
-        'margin-top': '8vh'
+        'margin-top': '5vh'
     });
     $('.month__img').css({
         // 'background': 'url(../img/smoker.png',
